e2f53454cd18e512079ae8220e06657639edaf23
jesse <jesse@JESSE>
2022-10-20 21:15:25 +0300

    Add project files.

---
+++ Cormorant1/Calc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cormorant1
+{
+    public class Calc
+    {
+        public static double Divide(double b, double y)
+        {
+            double answer = b / y;
+            return answer;
+        }
+
+        public static double Multiple(double b, double y)
+        {
+            double answer = b * y;
+            return answer;
+        }
+
+        public static double Add(double b, double y)
+        {
+            double answer = b + y;
+            return answer;
+        }
+
+        public static double Power(double b, double y)
+        {
+            double answer = Math.Pow(b, y);
+            return answer;
+        }
+    }
+}
+    
---
+++ Cormorant1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Cormorant1;
+double populationOfFinland = 5518000;
+double populationOfCormorants = 53400;
+double year1 = 2018;
+double populationGrowth = 1.08;
+double newPopulationGrowth = 1.001;
+
+
+
+double result1 = Calc.Divide(populationOfFinland, populationOfCormorants);
+double duration = Calc.Divide(Math.Log(result1), Math.Log(populationGrowth));
+double answerA = Calc.Add(year1, duration);
+answerA = Convert.ToInt32(answerA);
+Console.WriteLine("a) Number of cormorants equal to Finnish population " + answerA);
+Console.WriteLine(" ");
+
+double newGrowth = Calc.Power(newPopulationGrowth, duration);
+double newPopulationOfFinland = Calc.Multiple(populationOfFinland, newGrowth);
+double result3 = Calc.Divide(newPopulationOfFinland, populationOfCormorants);
+double result4 = Calc.Divide(Math.Log(result3), Math.Log(populationGrowth));
+double answerB = result4 + year1;
+answerB = Convert.ToInt32(answerB);
+Console.WriteLine("b) Number of cormorants equal to Finnish population " + answerB);
+
+
